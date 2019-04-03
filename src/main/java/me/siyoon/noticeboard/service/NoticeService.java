af1b10d2fc40450664a901d2481406afed0d9c7c
siyoon210 <siyoon210@gmail.com>
2019-04-03 14:38:50 +0900

    feat: NoticeService 인터페이스 생성, 메소드 설계

--- src/main/java/me/siyoon/noticeboard/service/NoticeService.java
+++ src/main/java/me/siyoon/noticeboard/service/NoticeService.java
@@ -1,16 +1,17 @@
 package me.siyoon.noticeboard.service;
 
 import me.siyoon.noticeboard.domain.Notice;
+import me.siyoon.noticeboard.dto.NoticeForm;
 import org.springframework.data.domain.Page;
 
 public interface NoticeService {
-    Page<Notice> getNoticePage(int startPage);
+    Page<Notice> getNoticePage(Integer startPage);
 
     Notice addNotice(NoticeForm noticeForm);
 
-    Notice getNotice();
+    Notice getNotice(Long id);
 
-    Notice modifyNotice();
+    Notice modifyNotice(NoticeForm noticeForm);
 
-    void deleteNotice();
+    void deleteNotice(Long id);
 }
