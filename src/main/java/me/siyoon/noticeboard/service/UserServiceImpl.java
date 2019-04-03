08cf511513186d56dcc8586ce45681f3a1666479
siyoon210 <siyoon210@gmail.com>
2019-04-03 16:18:47 +0900

    feat: User 조회 관련 메소드 구현

--- src/main/java/me/siyoon/noticeboard/service/UserServiceImpl.java
+++ src/main/java/me/siyoon/noticeboard/service/UserServiceImpl.java
@@ -1,17 +1,24 @@
 package me.siyoon.noticeboard.service;
 
+import lombok.RequiredArgsConstructor;
 import me.siyoon.noticeboard.domain.User;
 import me.siyoon.noticeboard.dto.UserForm;
+import me.siyoon.noticeboard.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
     @Override
     public User getUser(String email) {
-        return null;
+        return userRepository.findUserByEmail(email);
     }
 
     @Override
     public User getUser(Long id) {
-        return null;
+        return userRepository.findUserById(id);
     }
 
     @Override
