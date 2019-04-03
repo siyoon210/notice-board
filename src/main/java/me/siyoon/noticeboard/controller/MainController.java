986dde4d74cb945084d262ee021d0f0eaabca712
siyoon210 <siyoon210@gmail.com>
2019-04-03 14:57:30 +0900

    feat: MainController와 Index 페이지 생성

---
+++ src/main/java/me/siyoon/noticeboard/controller/MainController.java
@@ -0,0 +1,12 @@
+package me.siyoon.noticeboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String main() {
+        return "/index";
+    }
+}
