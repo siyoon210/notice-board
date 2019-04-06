abccebeb0c148c586feade1e8ae6e3f6c3df28a9
siyoon210 <siyoon210@gmail.com>
2019-04-06 21:50:21 +0900

    style: 프론트 디자인 완성

--- src/main/java/me/siyoon/noticeboard/domain/enums/PageSize.java
+++ src/main/java/me/siyoon/noticeboard/domain/enums/PageSize.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PageSize {
-    NOTICE(2, 2);
+    NOTICE(7, 5);
 
     private Integer content; //한페이지에 출력할 글의 갯수
     private Integer pageNavCount; //페이징 네비게이션에서 출력할 페이지의 갯수
